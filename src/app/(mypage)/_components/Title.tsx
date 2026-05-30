e289c8a11ecc266e87539b9b17244c058898b8a7
JuHeonParkk <ju10913@naver.com>
2026-05-30 16:00:20 +0900

    feat: 마이페이지 title 스타일링

---
+++ src/app/(mypage)/_components/Title.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/Button/Button";
+
+interface TitleProps {
+  title: string;
+  description: string;
+  buttonText?: string;
+  onButtonClick?: () => void;
+}
+
+const Title = ({
+  title,
+  description,
+  buttonText,
+  onButtonClick,
+}: TitleProps) => {
+  return (
+    <div className="flex flex-col justify-center items-start gap-3.5 mb-7.5 md:flex-row md:justify-between md:items-center">
+      <div className="flex flex-col justify-center items-start gap-2.5">
+        <h1 className="text-18-bold text-gray-950">{title}</h1>
+        <p className="text-14-medium text-gray-500">{description}</p>
+      </div>
+      {buttonText && (
+        <Button size="md" onClick={onButtonClick} className="w-full md:w-34.5">
+          {buttonText}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Title;
