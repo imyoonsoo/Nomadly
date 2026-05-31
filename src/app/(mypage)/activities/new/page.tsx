b1074451324971bfa920bef151158ebcdfc2176a
JuHeonParkk <ju10913@naver.com>
2026-06-01 00:58:04 +0900

    feat: 주소 서비스 버튼 추가

--- src/app/(mypage)/activities/new/page.tsx
+++ src/app/(mypage)/activities/new/page.tsx
@@ -5,9 +5,9 @@ import Button from "@/components/Button/Button";
 
 const CreateActivityForm = () => {
   return (
-    <div>
-      <h1>내 체험 등록</h1>
-      <form>
+    <div className="w-full">
+      <h1 className="py-5 text-18-bold text-gray-950">내 체험 등록</h1>
+      <form className="w-full flex flex-col justify-center gap-6 md:gap-7.5">
         <TextInput
           name="title"
           label="제목"
@@ -24,22 +24,32 @@ const CreateActivityForm = () => {
           name="description"
           label="설명"
           placeholder="체험에 대한 설명을 입력해 주세요"
+          textareaClassName="min-h-[140px] md:min-h-[200px]"
         />
+        {/* Todo: type에 number 필요 */}
         <TextInput
           name="price"
           label="가격"
           placeholder="체험 금액을 입력해 주세요"
         />
-        <TextInput
-          name="address"
-          label="주소"
-          placeholder="주소를 입력해 주세요"
-        />
+        <div className="flex items-end gap-3">
+          <TextInput
+            name="address"
+            label="주소"
+            placeholder="주소를 입력해 주세요"
+            className="flex-1"
+          />
+          <Button className="flex-0">주소 찾기</Button>
+        </div>
         <div>
           <p>예약 가능한 시간대</p>
         </div>
 
-        <MultiImageInput name="bannerImageUrl" label="배너 이미지 등록" />
+        <MultiImageInput
+          name="bannerImageUrl"
+          label="배너 이미지 등록"
+          maxCount={1}
+        />
         <MultiImageInput name="subImages" label="소개 이미지 등록" />
 
         <Button>등록하기</Button>
