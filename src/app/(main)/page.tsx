f07d8ad0bb73270572efeea07b9f421c0534ebfa
imyoonsoo <bliss38soo@gmail.com>
2026-06-01 22:57:37 +0900

    test: 버튼 사용 테스트코드 작성

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Button from "@/components/Button/Button";
+
+const TestpageButton = () => {
+  return (
+    <>
+      <Button variant="whitenBlue" height="h29">
+        후기 작성
+      </Button>
+    </>
+  );
+};
+
+export default TestpageButton;
