3a1bb90bdc48dd3eb1dfb1d4f3cb2a1376c5ff24
eugene <eugenekmpark@gmail.com>
2026-06-02 00:30:10 +0900

    style: 카드 컴포넌트 반응형 구현

--- src/app/(mypage)/reservations/components/ReservedCard.tsx
+++ src/app/(mypage)/reservations/components/ReservedCard.tsx
@@ -5,10 +5,10 @@ const ReservedCard = () => {
   return (
     <div className="pt-[20px] flex flex-col gap-[12px]">
       <p className="text-16-bold text-gray-800 lg:text-18-bold">2023.12.04</p>
-      <div className="relative rounded-[32px] w-[327px] h-[136px] overflow-hidden md:w-[476px] md:h-[136px] lg:w-[640px] lg:h-[181px] shadow-[0_4px_24px_0_rgba(156,180,202,0.20)]">
-        <div className="flex flex-col justify-between relative z-10 w-[229px] h-[136px] p-5 rounded-[32px] bg-white md:w-[360px] md:h-[136px] lg:w-[485px] lg:h-[181px] lg:px-[40px] lg:py-[30px] ">
+      <div className="relative rounded-[32px] h-[136px]  w-[100%] lg:w-[90%] lg:max-w-[640px] lg:h-[181px] overflow-hidden shadow-[0_4px_24px_0_rgba(156,180,202,0.20)]">
+        <div className="flex flex-col justify-between relative z-10 w-[70%] h-full p-5 rounded-[32px] bg-white lg:px-[40px] lg:py-[30px]">
           <StateBadge status="confirmed" />
-          <div className="flex flex-col ">
+          <div className="flex flex-col">
             <h1 className="text-14-bold text-gray-950 lg:text-18-bold">
               title
             </h1>
@@ -23,11 +23,11 @@ const ReservedCard = () => {
                 / 00명
               </span>
             </p>
-            <div className="hidden lg:flex gap-[8px]">
+            <div className="hidden lg:flex gap-[8px] ">
               <Button
                 styleVariant="fillWhite"
                 heightSize="h29"
-                className="h-[29px] px-[10px] py-[6px] rounded-lg text-14-medium !border"
+                className="px-[10px] py-[6px] !border"
               >
                 예약 변경
               </Button>
@@ -41,7 +41,7 @@ const ReservedCard = () => {
             </div>
           </div>
         </div>
-        <div className="absolute right-0  top-1/2 -translate-y-1/2 w-[137px] h-[135px] lg:w-[182px] lg:h-[180px] overflow-hidden">
+        <div className="absolute right-0 top-0 h-full w-[40%] overflow-hidden">
           <Image
             src="/exImg.svg"
             fill
@@ -50,11 +50,11 @@ const ReservedCard = () => {
           />
         </div>
       </div>
-      <div className="flex w-[327px] md:w-[476px] gap-[12px]  lg:hidden">
+      <div className="flex w-full gap-[12px] lg:hidden">
         <Button
           styleVariant="fillWhite"
           heightSize="custom"
-          className="flex-1 h-[37px] rounded-lg p-[10px] "
+          className="flex-1 h-[37px] rounded-lg p-[10px]"
         >
           예약 변경
         </Button>
