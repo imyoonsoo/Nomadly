7c9c0c31c3b07bd280b4b3b1664f501f0e41e949
eugene <eugenekmpark@gmail.com>
2026-06-01 13:38:02 +0900

    chore: 페이지 마진 추가

--- src/app/(mypage)/reservations/page.tsx
+++ src/app/(mypage)/reservations/page.tsx
@@ -5,10 +5,12 @@ const ReservationsPage = () => {
   return (
     <>
       <header className="flex flex-col gap-">
-        <h1 className="text-18-bold text-gray-950">예약 내역</h1>
-        <p className="text-14-medium text-gray-500">
-          예약내역 변경 및 취소할 수 있습니다.
-        </p>
+        <div>
+          <h1 className="text-18-bold text-gray-950 mb-[10px]">예약 내역</h1>
+          <p className="text-14-medium text-gray-500">
+            예약내역 변경 및 취소할 수 있습니다.
+          </p>
+        </div>
       </header>
       {/* Todo: 필터 버튼 추가 */}
       <ReservedCardList />
