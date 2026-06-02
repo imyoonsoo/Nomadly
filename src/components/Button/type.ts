97018b90f1b612efe985f6fbd8116d457b802bdc
imyoonsoo <bliss38soo@gmail.com>
2026-06-02 14:36:16 +0900

    refactor: 타입에 onlyGray, reviewMore 추가

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,7 +1,14 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "mainBlue" | "whitenBlue" | "whitenGray" | "clear" | "kakao";
+  variant?:
+    | "mainBlue"
+    | "whitenBlue"
+    | "whitenGray"
+    | "onlyGray"
+    | "clear"
+    | "kakao"
+    | "reviewMore";
 
   height?: "54lg" | "47md" | "41sm" | "h23" | "h29" | "h37" | "h50" | "custom";
   icon?: ReactNode;
