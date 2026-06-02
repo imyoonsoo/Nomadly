04d66d1b4afce2d733f4e8bcdfd114a37521b221
eugene <eugenekmpark@gmail.com>
2026-06-03 00:58:27 +0900

    feat: 목 데이터 추가 및 테스트

--- src/app/(mypage)/reservations/components/ReservedCard.tsx
+++ src/app/(mypage)/reservations/components/ReservedCard.tsx
@@ -1,44 +1,82 @@
 import Button from "@/components/Button/Button";
 import StateBadge from "@/components/StateBadge/StateBadge";
 import Image from "next/image";
-const ReservedCard = () => {
+
+export type ReservationStatus =
+  | "pending"
+  | "confirmed"
+  | "declined"
+  | "canceled"
+  | "completed";
+
+export interface ReservedCardProps {
+  date: string;
+  title: string;
+  status: ReservationStatus;
+  startTime: string;
+  endTime: string;
+  totalPrice: number;
+  headCount: number;
+  // Todo: bannerImage 추가
+}
+
+const ReservedCard = ({
+  date,
+  title,
+  status,
+  startTime,
+  endTime,
+  totalPrice,
+  headCount,
+}: ReservedCardProps) => {
   return (
     <div className="pt-[20px] flex flex-col gap-[12px]">
-      <p className="text-16-bold text-gray-800 lg:text-18-bold">2023.12.04</p>
+      <p className="text-16-bold text-gray-800 lg:text-18-bold">{date}</p>
       <div className="relative rounded-[32px] h-[136px]  w-[100%] lg:w-[90%] lg:max-w-[640px] lg:h-[181px] overflow-hidden shadow-[0_4px_24px_0_rgba(156,180,202,0.20)]">
         <div className="flex flex-col justify-between relative z-10 w-[70%] h-full p-5 rounded-[32px] bg-white lg:px-[40px] lg:py-[30px]">
-          <StateBadge status="confirmed" />
+          <StateBadge status={status} />
           <div className="flex flex-col">
             <h1 className="text-14-bold text-gray-950 lg:text-18-bold">
-              title
+              {title}
             </h1>
             <p className="text-13-medium text-gray-500 lg:text-16-medium">
-              11:00 - 12:30
+              {startTime} - {endTime}
             </p>
           </div>
           <div className="flex justify-between">
             <p className="text-16-bold text-gray-950 lg:text-18-bold">
-              ₩ 35,000
+              {totalPrice}
               <span className="text-14-medium text-gray-400 lg:text-16-medium">
-                / 00명
+                / {headCount}명
               </span>
             </p>
-            <div className="hidden lg:flex gap-[8px] ">
+            {status === "confirmed" && (
+              <div className="hidden lg:flex gap-[8px] ">
+                <Button
+                  variant="whitenGray"
+                  height="h29"
+                  className="px-[10px] py-[6px] !border"
+                >
+                  예약 변경
+                </Button>
+                <Button
+                  variant="onlyGray"
+                  height="custom"
+                  className="h-[29px] px-[10px] py-[6px] rounded-lg text-14-medium !text-gray-600"
+                >
+                  예약 취소
+                </Button>
+              </div>
+            )}
+            {status === "completed" && (
               <Button
-                variant="whitenGray"
-                height="h29"
-                className="px-[10px] py-[6px] !border"
-              >
-                예약 변경
-              </Button>
-              <Button
-                variant="onlyGray"
+                variant="mainBlue"
                 height="custom"
-                className="h-[29px] px-[10px] py-[6px] rounded-lg text-14-medium !text-gray-600"
+                className="hidden lg:block h-[29px] px-[10px] rounded-lg text-14-medium"
               >
-                예약 취소
+                후기 작성
               </Button>
-            </div>
+            )}
           </div>
         </div>
         <div className="absolute right-0 top-0 h-full w-[40%] overflow-hidden">
@@ -50,25 +88,34 @@ const ReservedCard = () => {
           />
         </div>
       </div>
-      <div className="flex w-full gap-[12px] lg:hidden">
+      {status === "confirmed" && (
+        <div className="flex w-full gap-[12px] lg:hidden">
+          <Button
+            variant="whitenGray"
+            height="custom"
+            className="flex-1 h-[37px] rounded-lg p-[10px]"
+          >
+            예약 변경
+          </Button>
+          <Button
+            variant="onlyGray"
+            height="custom"
+            className="flex-1 h-[37px] rounded-lg p-[10px]"
+          >
+            예약 취소
+          </Button>
+        </div>
+      )}
+      {status === "completed" && (
         <Button
-          variant="whitenGray"
+          variant="mainBlue"
           height="custom"
-          className="flex-1 h-[37px] rounded-lg p-[10px]"
+          className="lg:hidden sm:block md:block w-full h-[37px] px-[10px] rounded-[8px]"
         >
-          예약 변경
+          후기 작성
         </Button>
-        <Button
-          variant="onlyGray"
-          height="custom"
-          className="flex-1 h-[37px] rounded-lg p-[10px]"
-        >
-          예약 취소
-        </Button>
-      </div>
+      )}
     </div>
-
-    // Todo: 버튼 추가하기
   );
 };
 
--- src/app/(mypage)/reservations/components/ReservedCardList.tsx
+++ src/app/(mypage)/reservations/components/ReservedCardList.tsx
@@ -1,7 +1,60 @@
-import ReservedCard from "./ReservedCard";
+import ReservedCard, { ReservedCardProps } from "./ReservedCard";
 
 const ReservedCardList = () => {
-  return <ReservedCard />;
+  const mockReservations: ReservedCardProps[] = [
+    {
+      date: "2023.02.14",
+      title: "함께 배우면 즐거운 스트릿 댄스",
+      status: "confirmed",
+      startTime: "11:00",
+      endTime: "12:30",
+      totalPrice: 10000,
+      headCount: 10,
+    },
+    {
+      date: "2023.02.11",
+      title: "내 강아지 인생 사진 찍어주기",
+      status: "canceled",
+      startTime: "13:00",
+      endTime: "14:0",
+      totalPrice: 35000,
+      headCount: 10,
+    },
+    {
+      date: "2023.01.31",
+      title: "이색 앵무새와 친구 되기",
+      status: "declined",
+      startTime: "10:00",
+      endTime: "12:00",
+      totalPrice: 60000,
+      headCount: 3,
+    },
+    {
+      date: "2023.01.14",
+      title: "발리 코끼리 목욕 체험",
+      status: "completed",
+      startTime: "16:00",
+      endTime: "17:30",
+      totalPrice: 40000,
+      headCount: 2,
+    },
+    {
+      date: "2023.01.10",
+      title: "열기구 페스티벌",
+      status: "pending",
+      startTime: "10:00",
+      endTime: "12:30",
+      totalPrice: 70000,
+      headCount: 2,
+    },
+  ];
+  return (
+    <div className="flex flex-col gap-[30px]">
+      {mockReservations.map((reservation, index) => (
+        <ReservedCard key={index} {...reservation} />
+      ))}
+    </div>
+  );
 };
 
 export default ReservedCardList;
