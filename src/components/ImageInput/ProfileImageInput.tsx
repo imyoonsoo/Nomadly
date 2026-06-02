49f8bb05157c746b6a4b977d9acf819ea9c9c8a3
한병현 <gksqudgus777@naver.com>
2026-06-02 21:59:32 +0900

    design: tailwind 임의값 대신 추천 css 로 수정

--- src/components/ImageInput/ProfileImageInput.tsx
+++ src/components/ImageInput/ProfileImageInput.tsx
@@ -58,7 +58,7 @@ const ProfileImageInput = ({
             <DefaultProfile className="h-full w-full object-cover" />
           </div>
         )}
-        <div className="flex items-center justify-center absolute bottom-3 right-2.5 w-[30px] h-[30px] bg-gray-300 rounded-full">
+        <div className="flex items-center justify-center absolute bottom-3 right-2.5 w-7.5 h-7.5 bg-gray-300 rounded-full">
           <Edit className="w-4 h-4 text-white" />
         </div>
       </label>
