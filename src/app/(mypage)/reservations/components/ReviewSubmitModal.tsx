ba591e560efe9546484defc87529b2a37c58548f
eugene <eugenekmpark@gmail.com>
2026-06-02 20:09:19 +0900

    style: 리뷰 작성 모달 반응형 구현

--- src/app/(mypage)/reservations/components/ReviewSubmitModal.tsx
+++ src/app/(mypage)/reservations/components/ReviewSubmitModal.tsx
@@ -4,6 +4,7 @@ import StarOn from "@/assets/icons/star-on.svg";
 import StarOff from "@/assets/icons/star-off.svg";
 import { useState } from "react";
 import TextArea from "@/components/Input/TextArea";
+import Button from "@/components/Button/Button";
 
 interface ReviewSubmitModalProps {
   isOpen: boolean;
@@ -15,7 +16,12 @@ const ReviewSubmitModal = ({ isOpen, onClose }: ReviewSubmitModalProps) => {
 
   return (
     <>
-      <Modal size="lg" isOpen={isOpen} onClose={onClose}>
+      <Modal
+        size="lg"
+        isOpen={isOpen}
+        onClose={onClose}
+        className="w-[327px] h-[479px]"
+      >
         <div className="flex flex-col gap-[20px]">
           <div className="flex flex-col gap-[14px]">
             <div className="flex flex-col gap-[8px]">
@@ -24,10 +30,10 @@ const ReviewSubmitModal = ({ isOpen, onClose }: ReviewSubmitModalProps) => {
                   <DeleteIcon width={24} height={24} />
                 </button>
               </div>
-              <p className="text-14-bold text-center">
+              <p className="text-14-bold text-center md:text-16-bold">
                 함께 배우면 즐거운 스트릿 댄스
               </p>
-              <p className="text-13-medium text-gray-500 text-center">
+              <p className="text-13-medium text-gray-500 text-center md:text-14-medium">
                 2023.02.14 / 11:00 - 12:30 (10명)
               </p>
             </div>
@@ -35,9 +41,17 @@ const ReviewSubmitModal = ({ isOpen, onClose }: ReviewSubmitModalProps) => {
               {[1, 2, 3, 4, 5].map((star) => (
                 <button key={star} onClick={() => setRating(star)}>
                   {star <= rating ? (
-                    <StarOn width={36} height={36} />
+                    <StarOn
+                      width={36}
+                      height={36}
+                      className="md:w-[42px] md:h-[42px]"
+                    />
                   ) : (
-                    <StarOff width={36} height={36} />
+                    <StarOff
+                      width={36}
+                      height={36}
+                      className="md:w-[42px] md:h-[42px]"
+                    />
                   )}
                 </button>
               ))}
@@ -51,9 +65,14 @@ const ReviewSubmitModal = ({ isOpen, onClose }: ReviewSubmitModalProps) => {
             textCount
             textareaClassName="h-[179px]"
           />
+          <Button
+            variant="mainBlue"
+            height="custom"
+            className="w-full h-[41px] rounded-[12px] md:h-[54px]"
+          >
+            작성하기
+          </Button>
         </div>
-
-        <div>{/* Todo: 버튼 */}</div>
       </Modal>
     </>
   );
