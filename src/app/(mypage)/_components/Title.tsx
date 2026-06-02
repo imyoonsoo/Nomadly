fb46cc2868d3691550d63f43d215c37a47bfcfde
JuHeonParkk <ju10913@naver.com>
2026-06-02 20:39:40 +0900

    fix: button props 변경

--- src/app/(mypage)/_components/Title.tsx
+++ src/app/(mypage)/_components/Title.tsx
@@ -14,17 +14,17 @@ const Title = ({
   onButtonClick,
 }: TitleProps) => {
   return (
-    <div className="flex flex-col justify-center items-start gap-3.5 mb-7.5 md:flex-row md:justify-between md:items-center">
+    <div className="flex flex-col justify-center items-start gap-3.5 mb-3.25 md:mb-7.5 md:flex-row md:justify-between md:items-center">
       <div className="flex flex-col justify-center items-start gap-2.5">
         <h1 className="text-18-bold text-gray-950">{title}</h1>
         <p className="text-14-medium text-gray-500">{description}</p>
       </div>
       {buttonText && (
         <Button
-          styleVariant="fillPrimaryBlue"
-          heightSize="md47"
+          variant="mainBlue"
+          height="47md"
           onClick={onButtonClick}
-          className="w-full md:w-34.5"
+          className="w-full md:w-auto"
         >
           {buttonText}
         </Button>
