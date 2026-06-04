796419007d37718cee1f8a355adc98863775fd60
한병현 <gksqudgus777@naver.com>
2026-06-05 03:21:51 +0900

    feat: Description Section 구현

---
+++ src/app/(main)/activities/[id]/components/DescriptionSection/type.ts
@@ -0,0 +1,5 @@
+import { ActivityDetailResponse } from "@/app/(main)/activities/type";
+
+type DescriptionSectionProps = Pick<ActivityDetailResponse, "description">;
+
+export default DescriptionSectionProps;
