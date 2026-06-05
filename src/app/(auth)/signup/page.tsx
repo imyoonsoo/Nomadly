f4126d15e5b0e5900e0bdc02863c623a33e5128b
imyoonsoo <bliss38soo@gmail.com>
2026-06-06 07:08:52 +0900

    feat: 회원가입 페이지 반응형 퍼블리싱 포함 UI 구현

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import ValidationSignupForm from "@/features/signup/ValidationSignupForm/ValidationSignupForm";
+
+const SignupPage = () => {
+  return <ValidationSignupForm></ValidationSignupForm>;
+};
+
+export default SignupPage;
