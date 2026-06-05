abbea1449ce675a6d7aa3109f7e6e0dd6ea9fcb0
imyoonsoo <bliss38soo@gmail.com>
2026-06-05 13:31:48 +0900

    add: 마이페이지 닉네임, 비밀번호, 프로필이미지 수정폼 mock데이터 추가

---
+++ src/features/mypage/mock.ts
@@ -0,0 +1,42 @@
+// 테스트용 mock데이터 (추후 useQuery로 교체 예정)
+const mockDataList = [
+  // mockDataList: 경민님 ~ 병현님
+  {
+    id: 1,
+    nickname: "FE_23기_박경민",
+    email: "Eugenekmp@codeit.com",
+    profileImageUrl: "",
+  },
+  {
+    id: 2,
+    nickname: "FE_23기_박주헌",
+    email: "JuHeonParkk@codeit.com",
+    profileImageUrl: "",
+  },
+  {
+    id: 2,
+    nickname: "융수",
+    email: "imyoonsoo@codeit.com",
+    profileImageUrl: "",
+  },
+  {
+    id: 4,
+    nickname: "FE_23기_이은지",
+    email: "ejlee6742@codeit.com",
+    profileImageUrl: "",
+  },
+  {
+    id: 5,
+    nickname: "현서",
+    email: "hhhnseo@codeit.com",
+    profileImageUrl: "",
+  },
+  {
+    id: 6,
+    nickname: "한병현",
+    email: "Hanbh97@codeit.com",
+    profileImageUrl: "",
+  },
+];
+
+export default mockDataList;
