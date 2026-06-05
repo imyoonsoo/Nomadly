9c497baac1f549ee6f1f8c5cab29aeb26775ab3f
imyoonsoo <bliss38soo@gmail.com>
2026-06-06 00:00:22 +0900

    feat: 추가한 svg 맞춰서 변경

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,19 @@
 import DefaultProfile from "@/assets/images/default-profile.svg";
+import LogoPcTb from "@/assets/images/logo-pctablet-withtext.svg";
+import LogoMobile from "@/assets/images/logo-mobile-onlyimage.svg";
+import LogoGlobalNomadText from "@/assets/images/text-globalnomad.svg";
 import Empty from "@/assets/images/empty.svg";
 import MockImage1 from "@/assets/images/mock-image1.png";
 import MockImage2 from "@/assets/images/mock-image2.png";
 import MockImage3 from "@/assets/images/mock-image3.png";
 
-export { DefaultProfile, Empty, MockImage1, MockImage2, MockImage3 };
+export {
+  DefaultProfile,
+  Empty,
+  MockImage1,
+  MockImage2,
+  MockImage3,
+  LogoPcTb,
+  LogoMobile,
+  LogoGlobalNomadText,
+};
