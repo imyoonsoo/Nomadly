07d9e1f7e5b4b9329f5855ec0f2c96f69d099a5c
imyoonsoo <bliss38soo@gmail.com>
2026-06-06 09:43:49 +0900

    refactor: 위아래 패딩 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,17 @@
 import ValidationLoginForm from "@/features/login/components/ValidationLoginForm/ValidationLoginForm";
 
 const LoginPage = () => {
-  return <ValidationLoginForm></ValidationLoginForm>;
+  return (
+    <div
+      className="flex items-center justify-center w-full 
+      pt-10 pb-20 px-6 
+    md:pt-20 md:pb-25 md:px-13
+    lg:px-0    
+    "
+    >
+      <ValidationLoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
