9a9419ade2fb79fcfb7b5aa8c4389dab52eed37c
imyoonsoo <bliss38soo@gmail.com>
2026-06-07 14:13:23 +0900

    refactor: 정렬 프롭 제거, 스타일 개선, 주석 추가

--- src/features/signup/ValidationSignupForm/ValidationSignupForm.tsx
+++ src/features/signup/ValidationSignupForm/ValidationSignupForm.tsx
@@ -163,7 +163,7 @@ const ValidationSignupForm = () => {
       {/* 카카오 간편 회원가입하기 */}
       <Button
         type="button"
-        variant="easykakao"
+        variant="easyKakao"
         height="54lg"
         className="self-stretch"
         onClick={() => {
