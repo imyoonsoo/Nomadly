c23ae7240bb7516d8af5ffb066228f8a54adca70
imyoonsoo <bliss38soo@gmail.com>
2026-06-10 10:49:57 +0900

    feat: 스웨거 구조에 맞춰 request, response 정의

--- src/features/signup/type.ts
+++ src/features/signup/type.ts
@@ -4,3 +4,18 @@ export interface SignupFormValues {
   password: string;
   passwordConfirm: string;
 }
+
+export interface SignupRequestBody {
+  email: string;
+  nickname: string;
+  password: string;
+}
+
+export interface SignupResponse {
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
