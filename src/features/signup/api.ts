829ff55c07a24277a6898b28c53ff48e8a5a0a2f
imyoonsoo <bliss38soo@gmail.com>
2026-06-11 01:05:08 +0900

    fix: 1개만 내보내니 export default로 변경

--- src/features/signup/api.ts
+++ src/features/signup/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
-
 import { SignupRequestBody, SignupResponse } from "./type";
 
-export const postSignup = async (body: SignupRequestBody) => {
+const postSignup = async (body: SignupRequestBody) => {
   const response = await axios.post<SignupResponse>(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/users`,
     body,
   );
   return response.data;
 };
+
+export default postSignup;
