a8e842c55d287c59aa607986e0fc413109632d4a
eugene <eugenekmpark@gmail.com>
2026-06-11 01:22:53 +0900

    feat: clientfetch 리다이렉션 추가

--- src/lib/http/client-fetch.ts
+++ src/lib/http/client-fetch.ts
@@ -2,8 +2,15 @@ import axios from "axios";
 
 export const clientFetch = axios.create({
   baseURL: "/api/proxy",
-  headers: {
-    "Content-Type": "application/json",
-  },
   withCredentials: true,
 });
+
+clientFetch.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
