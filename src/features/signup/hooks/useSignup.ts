2dd63434c1dd2307a136cab0dcd73ab451b72a3d
imyoonsoo <bliss38soo@gmail.com>
2026-06-11 01:07:11 +0900

    fix: hooks 폴더 postSignup 임포트에서 중괄호 제거

--- src/features/signup/hooks/useSignup.ts
+++ src/features/signup/hooks/useSignup.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { postSignup } from "../api";
+import postSignup from "../api";
 
 export const useSignup = () => {
   return useMutation({
