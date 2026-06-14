f5d06f1946ff2f7a6e7d4d239c8663d3aa103992
eugene <eugenekmpark@gmail.com>
2026-06-14 18:10:55 +0900

    feat: 예약 없을 때 나타날 페이지

--- src/features/reservations/components/ReservedCardList.tsx
+++ src/features/reservations/components/ReservedCardList.tsx
@@ -4,6 +4,9 @@ import FilterButton from "@/components/FilterButton/FilterButton";
 import ReservedCard from "./ReservedCard";
 import { useQuery } from "@tanstack/react-query";
 import { myReservationsQuery } from "@/features/reservations/queries";
+import { useRouter } from "next/navigation";
+import EmpytyIcon from "@/assets/images/empty.svg";
+import Button from "@/components/Button/Button";
 
 const FILTERS = [
   "예약 대기",
@@ -22,6 +25,8 @@ const FILTER_STATUS_MAP: Record<string, string> = {
 };
 
 const ReservedCardList = () => {
+  const router = useRouter();
+
   const [activeFilter, setActiveFilter] = useState<string | null>(null);
 
   const activeStatus = activeFilter
@@ -39,6 +44,25 @@ const ReservedCardList = () => {
   const reservations = data?.reservations ?? [];
 
   if (isLoading) return <div>로딩 중...</div>;
+
+  if (!data?.reservations?.length) {
+    return (
+      <div className="w-[100%] h-[100%] mt-[10px] flex flex-col gap-[30px] justify-center items-center">
+        <div>
+          <EmpytyIcon width={180} height={203} />
+          <p>아직 예약된 체험이 없어요</p>
+        </div>
+        <Button
+          variant="mainBlue"
+          height="custom"
+          className="w-[182px] h-[54px] rounded-[16px] text-16-bold"
+          onClick={() => router.push("/")}
+        >
+          둘러보기
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-[30px]">
