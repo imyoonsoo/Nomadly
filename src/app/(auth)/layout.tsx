df74badb81152411cdd1beae7a2cd8e236f32c75
imyoonsoo <bliss38soo@gmail.com>
2026-06-20 06:42:13 +0900

    fix: Header, Footer 제거 및 padding 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,16 @@
 import Link from "next/link";
 import { LogoPcTablet, LogoMobile } from "@/constants/images";
-import Header from "@/components/layout/Header/Header";
-import Footer from "@/components/layout/Footer/Footer";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-dvh">
-      <Header />
       <main
         className="flex-1 flex items-center justify-center w-full bg-white
-        pt-25 pb-30 px-6
-        md:pt-35 md:mb-30 md:px-13 md:pb-30
+        pt-16 pb-36
+        md:pt-35 md:pb-30 md:px-13
         lg:px-0"
       >
-        <div className="w-full md:w-160 lg:w-160 mx-auto flex flex-col items-center gap-4 md:gap-5 px-6 md:px-0">
+        <div className="w-full md:w-160 mx-auto flex flex-col items-center gap-10 md:gap-16 px-6 md:px-0">
           <Link
             href="/"
             aria-label="메인페이지"
@@ -29,7 +26,6 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
           {children}
         </div>
       </main>
-      <Footer />
     </div>
   );
 };
