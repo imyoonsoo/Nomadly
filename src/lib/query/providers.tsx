d31dfedee869d085a76315aeafe6c5833ece96ec
eugene <eugenekmpark@gmail.com>
2026-06-19 13:54:45 +0900

    refactor: query provider 수정

--- src/lib/query/providers.tsx
+++ src/lib/query/providers.tsx
@@ -2,17 +2,15 @@
 
 import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
 import UserSessionHydrator from "@/features/auth/components/UserSessionHydrator";
-import { makeQueryClient } from "./get-query-client";
+import { getQueryClient, makeQueryClient } from "./get-query-client";
 import { useRef } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const clientRef = useRef<QueryClient | null>(null);
-  if (!clientRef.current) {
-    clientRef.current = makeQueryClient();
-  }
+  const queryClient = getQueryClient();
+
   return (
-    <QueryClientProvider client={clientRef.current}>
+    <QueryClientProvider client={queryClient}>
       <UserSessionHydrator>{children}</UserSessionHydrator>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
