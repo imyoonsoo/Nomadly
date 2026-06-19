79a4c0c5c34b8a2f8796925e23361c9e289e6b9d
eugene <eugenekmpark@gmail.com>
2026-06-19 14:37:33 +0900

    chore: 제미나이 리뷰 반영

--- src/lib/query/providers.tsx
+++ src/lib/query/providers.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import UserSessionHydrator from "@/features/auth/components/UserSessionHydrator";
-import { getQueryClient, makeQueryClient } from "./get-query-client";
-import { useRef } from "react";
+import { getQueryClient } from "./get-query-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
